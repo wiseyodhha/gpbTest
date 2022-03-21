d4c548741261826eccc7d6da5f03221387d39952
wiseyodhha <dkitaev88@gmail.com>
2022-03-22 00:01:25 +0300

    test commit -a

--- sandbox/src/main/java/firstJava.java
+++ sandbox/src/main/java/firstJava.java
@@ -1,7 +1,6 @@
 public class firstJava {
-
-  public static void main (String[] args) {
-System.out.println("Hello, world!");
+  public static void main (String[] args){
+    System.out.println("Hello, world!");
   }
 
 }
