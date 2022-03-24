a4e0a542fae7d8b112b4b25aa2a6fa5a69f59136
wiseyodhha <dkitaev88@gmail.com>
2022-03-24 22:55:40 +0300

    new class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/firstJava.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/firstJava.java
@@ -6,26 +6,16 @@ public class firstJava {
     hello ("user");
     hello ("Dima");
 
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
-
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area (double l) {
-      return l * l;
-    }
-
-    public static double area (double a, double b) {
-    return a * b;
     }
 
 
