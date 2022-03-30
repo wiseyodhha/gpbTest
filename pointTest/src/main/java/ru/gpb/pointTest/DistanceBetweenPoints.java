29f4838a41ff6b9c8b820cb829cad41574461c26
wiseyodhha <dkitaev88@gmail.com>
2022-03-30 22:20:39 +0300

    refactoring

--- pointTest/src/main/java/ru/gpb/pointTest/DistanceBetweenPoints.java
+++ pointTest/src/main/java/ru/gpb/pointTest/DistanceBetweenPoints.java
@@ -2,8 +2,12 @@ package ru.gpb.pointTest;
 
 public class DistanceBetweenPoints {
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+ //   public static double distance(Point p1, Point p2) {
+ //       return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+ //   }
+
+    public static double distance2(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.y - p1.y), 2)  + Math.pow((p2.x - p1.x), 2));
     }
 
 }
--- pointTest/src/main/java/ru/gpb/pointTest/SecondJava.java
+++ pointTest/src/main/java/ru/gpb/pointTest/SecondJava.java
@@ -3,10 +3,11 @@ package ru.gpb.pointTest;
 public class SecondJava {
 
     public static void main(String[] args) {
-        Point p1 = new Point(2.3, -3.5);
-        Point p2 = new Point(-5.7, 8.1);
+        Point p1 = new Point(4, 2);
+        Point p2 = new Point(3, 5);
 
-        System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ")" + " и (" + p2.x + ", " + p2.y + ") = " + DistanceBetweenPoints.distance(p1, p2));
+  //      System.out.println("Расстояние между точками " + "(" + p1.x + ", " + p1.y + ")" + " И " + "(" + p2.x + ", " + p2.y + ")" + " = " + DistanceBetweenPoints.distance(p1, p2));
 
+        System.out.println("Расстояние между точками " + "(" + p1.x + ", " + p1.y + ")" + " И " + "(" + p2.x + ", " + p2.y + ")" + " = " + DistanceBetweenPoints.distance2(p1, p2));
     }
 }
